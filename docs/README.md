6de66d48028c62c8ac90b0d5b6e984a2c67c2de1
MrAsparag00se <gabriellimyiwei@gmail.com>
2025-01-21 13:52:34 +0800

    Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Vegetables User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Vegetables.java
@@ -0,0 +1,19 @@
+public class Vegetables {
+    public static void main(String[] args) {
+        String logo = "   ######   \n"
+                + "  ########  \n"
+                + " ########## \n"
+                + "    ||||    \n"
+                + "    ||||    \n"
+                + "    ||||    \n";
+        System.out.println("Hello from\n" + logo);
+
+        // Chatbot greeting and exit message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Vegetables");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
